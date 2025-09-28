e2858279327dbe66ec04a9af58b9f2ffd9ccbc99
edgardopaz <dedgardo381@gmail.com>
2025-09-28 03:28:23 -0400

    made several updates

--- mysite.py
+++ mysite.py
@@ -2,17 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", initial_section=None)
+
 
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template("index.html", initial_section="projects")
+
 
 @app.route("/about-me")
 def about_me():
-    return render_template("about_me.html")
+    return render_template("index.html", initial_section="about")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
