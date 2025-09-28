b95aca5b84252570761666ac771636cbd495584e
edgardopaz <dedgardo381@gmail.com>
2025-09-28 04:41:10 -0400

    fixing ports

--- mysite.py
+++ mysite.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -19,4 +21,5 @@ def about_me():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
